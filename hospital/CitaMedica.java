d18e7b2f6b7052acae800f07343fa6d9b52d700c
Emilio Jaramillo <166522792+emilioj04@users.noreply.github.com>
2024-05-21 09:03:36 -0500

    Getion Hospital
    JAVA

---
+++ hospital/CitaMedica.java
@@ -0,0 +1,12 @@
+public class CitaMedica implements GestionCItas {
+
+	Doctor doctor;
+	Paciente paciente;
+	Enfermero enfermero;
+	private Date fecha;
+	private Time hora;
+	private String motivoCita;
+	private estadoCita estado;
+	private Int contadorCitas;
+
+}
